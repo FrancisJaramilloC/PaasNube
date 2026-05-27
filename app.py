5ce41f7ce8718de8e17588f10d988e727c13e79a
Francisco Jaramillo <francisco.jaramillo@unl.edu.ec>
2026-05-27 00:44:03 -0500

    azure

---
+++ app.py
@@ -0,0 +1,4 @@
+from app.main import app
+
+# Este archivo sirve como punto de entrada raíz para que plataformas PaaS como Azure
+# detecten automáticamente la aplicación FastAPI sin configuraciones complejas.
